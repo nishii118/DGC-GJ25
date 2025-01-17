9387a3d1e3fc3e857accee05a81804987f1fc4e4
Nolan <poro2420033@gmail.com>
2025-01-17 23:50:29 +0700

    Player movement

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speedMove = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] Rigidbody2D rb2d;
+    [SerializeField] private Camera cam;
+    [SerializeField] private bool isUnderWater = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovingAndJumping();
+    }
+
+    private void MovingAndJumping()
+    {
+        // Set movement speed based on environment
+        speedMove = isUnderWater ? 5f : 2f;
+
+        // Get input values
+        float vertical = Input.GetAxis("Vertical");
+        float jump = isUnderWater ? Input.GetAxis("Jump") : 0f; // Jump only applies underwater
+        float horizontal = isUnderWater ? 2f : 1f;
+        // Combine horizontal and vertical/jump movement
+        Vector2 movement = new Vector2(horizontal, (vertical * speedMove) + (jump * jumpForce));
+        rb2d.velocity = movement;
+
+        // Uncomment if needed
+        // CameraFollowPlayer();
+    }
+
+    private void CameraFollowPlayer()
+    {
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+}
